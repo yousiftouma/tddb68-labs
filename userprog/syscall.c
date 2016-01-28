5b1fc2a228ed57f435d4cd9eca283983846fbe6b
holmgr <viktor.holmgren@gmail.com>
2016-01-28 15:30:00 +0100

    Implement write syscall to console

--- userprog/syscall.c
+++ userprog/syscall.c
@@ -15,9 +15,23 @@ syscall_init (void)
 static void
 syscall_handler (struct intr_frame *f UNUSED) 
 {
-  switch (*(int*) (f->esp)) {
-  	case SYS_HALT:
+  int syscall_nr = *(int*)(f->esp);
+  void* arg_pr = (f->esp);
+  switch (syscall_nr) {
+  	
+  	// System halt
+  	case SYS_HALT :
   		power_off();
+  		break;
+
+  	case SYS_WRITE:
+
+  		// Write to console
+  		if (((int*)arg_pr)[1] == 1) {
+  			const char* buf = ((char**)arg_pr)[2];
+  			size_t buf_size = ((size_t*)arg_pr)[3];
+  			putbuf(buf, buf_size);
+		}
   		break;
   	default:
   		printf("Unknown system call, %d", *(int*)f->esp);
