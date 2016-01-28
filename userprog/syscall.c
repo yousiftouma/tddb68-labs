594f37bc323716559ca4ec3ab536e3dddace2772
holmgr <viktor.holmgren@gmail.com>
2016-01-28 17:23:48 +0100

    Implement open syscall

--- userprog/syscall.c
+++ userprog/syscall.c
@@ -41,6 +41,15 @@ syscall_handler (struct intr_frame *f UNUSED)
   		success = filesys_create(((char**)arg_pr)[1], ((int*)arg_pr)[2]);
   		f->eax = success;
   		break;
+
+  	case SYS_OPEN:
+      ;
+      char* file_name = ((char**)arg_pr)[1];
+      struct file *new_open = filesys_open(file_name);
+      int fnd = bitmap_scan_and_flip (thread_current()->file_ids, 0, 1, 0);
+      thread_current()->open_files[fnd] = new_open;
+      f->eax = fnd;
+
   	default:
   		printf("Unknown system call, %d", *(int*)f->esp);
   }
